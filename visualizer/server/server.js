209ac5f269e388a67e453ea58fe26e67f028da39
novafacing <rowanbhart@gmail.com>
2020-04-28 11:31:05 -0400

    Can chonkify malloc_state and chonks now

--- visualizer/server/server.js
+++ visualizer/server/server.js
@@ -7,12 +7,41 @@ var path = require('path');
 io.use(middleware);
 const web = io.of("/web");
 const gef = io.of("/gef");
-var state = {};
-var glibcVersion = -1;
+var state = {
+  groups: [
+    {
+      name: 'tcache',
+      chunks: []
+    },
+    {
+      name: 'fastbins',
+      chunks: []
+    },
+    {
+      name: 'unsorted',
+      chunks: []
+    },
+    {
+      name: 'small',
+      chunks: []
+    },
+    {
+      name: 'large',
+      chunks: []
+    },
+    {
+      name: 'free',
+      chunks: []
+    }
+  ]
+};
+var initialized = false;
+/* Defaults, these are updated */
+var glibcVersion = 2.31;
+var ptrSize = 8;
 
 server.listen(3000);
 
-var initialized = false;
 
 function initClient(numGroups) {
   // libcVersion / something else to help me init the number of groups with names of each for the network
@@ -112,34 +141,270 @@ web.on("connection", function(socket) {
   });
 });
 
+function minChunkSize () {
+  return offsetOf(mallocChunk(), 'fd_nextsize');
+}
+
+function minSize () {
+  var constants = getStaticConstants();
+  return (minChunkSize() + constants.malloc_align_mask & ~constants.malloc_align_mask)
+}
+
+function request2size (req) {
+  var constants = getStaticConstants();
+  return (((req) + constants.size_sz + constants.malloc_align_mask < minSize()) ? minSize() : ((req) + constants.size_sz + constants.malloc_align_mask) & ~malloc_align_mask)
+}
+
+function fastbin_index(sz) {
+  var constants = getStaticConstants();
+  return (((sz) >> (constants.size_sz == 8 ? 4 : 3)) - 2)
+}
+
+function getConstants () {
+  var constants = getStaticConstants();
+  constants.nfastbins = fastbin_index(constants.max_fast_size);
+  return constants;
+}
+
+function getStaticConstants () {
+  var size_sz = ptrSize;
+  var max_fast_size = 80 * size_sz / 4;
+  var malloc_alignment = 2 * size_sz;
+  var malloc_align_mask = malloc_alignment - 1;
+  var nbins = 128;
+  var binmapshift = 5;
+  var bitspermap = 1 << binmapshift;
+  var binmapsize = nbins / bitspermap;
+  return {
+    'size_sz': size_sz,
+    'max_fast_size': max_fast_size,
+    'malloc_alignment': malloc_alignment,
+    'malloc_align_mask': malloc_align_mask,
+    'nbins': nbins,
+    'binmapshift': binmapshift,
+    'bitspermap': bitspermap,
+    'binmapsize': binmapsize,
+  }
+}
+
+function offsetOf(proto, field) {
+  let offset = 0;
+  for (let key in proto) {
+    if (key === field) {
+      return offset;
+    }
+    offset += proto.key.size * proto.key.count;
+  }
+  return offset;
+}
+
+function mallocChunk () {
+  return {
+    mchunk_prev_size: {
+      size: ptrSize,
+      count: 1
+    },
+    mchunk_size: {
+      size: ptrSize,
+      count: 1
+    },
+    fd: {
+      size: ptrSize,
+      count: 1
+    },
+    bk: {
+      size: ptrSize,
+      count: 1
+    },
+    fd_nextsize: {
+      size: ptrSize,
+      count: 1
+    },
+    bk_nextsize: {
+      size: ptrSize,
+      count: 1
+    }
+  };
+}
+
+function mallocState () {
+  var constants = getConstants();
+  return {
+    mutex: {
+      size: ptrSize / 2,
+      count: 1
+    },
+    flags: {
+      size: ptrSize,
+      count: 1
+    },
+    have_fastchunks: {
+      size: ptrSize,
+      count: 1
+    },
+    fastbinsY: {
+      size: ptrSize,
+      count: constants.nfastbins
+    },
+    top: {
+      size: ptrSize,
+      count: 1
+    },
+    last_remainder: {
+      size: ptrSize,
+      count: 1
+    },
+    bins: {
+      size: ptrSize,
+      count: constants.nbins * 2 - 2
+    },
+    binmap: {
+      size: ptrSize,
+      count: constants.binmapsize
+    },
+    next: {
+      size: ptrSize,
+      count: 1
+    },
+    next_free: {
+      size: ptrSize,
+      count: 1
+    },
+    attached_threads: {
+      size: ptrSize,
+      count: 1
+    },
+    system_mem: {
+      size: ptrSize,
+      count: 1
+    },
+    max_system_mem: {
+      size: ptrSize,
+      count: 1
+    }
+
+  };
+}
+
+function condense (raw, prototype) {
+  console.log('condensing ', raw, ' into ', prototype);
+  let condensed = {};
+  let loc = 0;
+  switch(prototype) {
+    case 'malloc_chunk':
+      let chunk = mallocChunk();
+      for (let member in chunk) {
+        condensed[member] = raw.slice(loc, loc + (chunk[member].size * chunk[member].count));
+        loc += chunk[member].size * chunk[member].count;
+      }
+      return condensed;
+      break;
+    case 'malloc_state':
+      let state = mallocState()
+      for (let member in state) {
+        condensed[member] = raw.slice(loc, loc + (state[member].size * state[member].count));
+        loc += state[member].size * state[member].count;
+      }
+      return condensed;
+      break;
+  }
+}
+
+function gefAction (sk, st, data) {
+  console.log('Got heap change event with data ', data);
+  switch(data['called-function']) {
+    case 'malloc':
+      malloc(sk, st, data);
+      break;
+    case 'calloc':
+      calloc(sk, st, data);
+      break;
+    case 'realloc':
+      realloc(sk, st, data);
+      break;
+    case 'free':
+      free(sk, st, data);
+      break;
+  };
+}
+
 gef.on("connect", function(socket) {
   socket.on('*', function (data) {
     console.log('Got unknown callback with data ', data);
   });
   socket.on("heap_changed", (data) => {
-    console.log('Got heap change event with data ', data);
-    switch(data['called-function']) {
-      case 'malloc':
-        malloc(socket, state, data);
-        break;
-      case 'calloc':
-        calloc(socket, state, data);
-        break;
-      case 'realloc':
-        realloc(socket, state, data);
-        break;
-      case 'free':
-        free(socket, state, data);
-        break;
-    };
+    if (!initialized) {
+      glibcVersion = getVersionNumber(socket).then((vnum) => {
+        getPtrSize(socket).then((ptsize) => {
+          getMainArenaAddr(socket).then((main_arena) => {
+            getMainArenaSize(socket, main_arena).then((main_arena_size) => {
+              getMainArenaContents(socket, main_arena, main_arena_size).then((main_arena_contents) => {
+                glibcVersion = vnum;
+                ptrSize = ptsize;
+                state = condense(changeEndianness(main_arena_contents), 'malloc_state');
+                console.log('Got state ', state);
+                initialized = true;
+                gefAction(socket, state, data);
+              });
+            });
+          });
+        });
+      });
+    } else {
+      gefAction(socket, state, data);
+    }
   });
   console.log('Got connection from gef');
-  glibcVersion = getVersionNumber(socket).then((data) => {
-    socket.emit("continue_execution");
-    return data;
-  });
-  state = {};
+  socket.emit('continue_execution');
 });
+
+function getPtrSize (socket) {
+  return new Promise((resolve, reject) => {
+    if (!socket) {
+      reject('No connection.');
+    } else {
+      socket.emit('sizeof', { var: '&main_arena' }, (data) => {
+        resolve(data.result);
+      });
+    }
+  });
+}
+
+function getMainArenaSize (socket) {
+  return new Promise((resolve, reject) => {
+    if (!socket) {
+      reject('No connection.');
+    } else {
+      socket.emit('sizeof', { var: 'main_arena' }, (data) => {
+        resolve(data.result);
+      });
+    }
+  });
+}
+
+function getMainArenaContents (socket, addr, size) {
+  return new Promise((resolve, reject) => {
+    if (!socket) {
+      reject('No connection.');
+    } else {
+      socket.emit('read_from_address', { address: addr, size: size }, (data) => {
+        resolve(data.result);
+      });
+    }
+  });
+} 
+
+function getMainArenaAddr (socket) {
+  return new Promise((resolve, reject) => {
+    if (!socket) {
+      reject('No connection.');
+    } else {
+      socket.emit('address_of_symbol', { symbol_name: 'main_arena' }, (data) => {
+        resolve(data.result);
+      });
+    }
+  });
+}
 
 function getVersionNumber (socket) {
   /* call the version number emit */
@@ -148,22 +413,20 @@ function getVersionNumber (socket) {
       reject('No connection.');
     } else {
       socket.emit('libc_version', (data) => {
-        resolve(data);
+        resolve(data.result);
       });
     }
   });
 }
 
 const changeEndianness = (string) => {
-        console.log('reverse-ending ', string);
-        const result = [];
-        let len = string.length - 2;
-        while (len >= 0) {
-          result.push(string.substr(len, 2));
-          len -= 2;
-        }
-        console.log('end changed ', result.join(''));
-        return result.join('');
+  const result = [];
+  let len = string.length - 2;
+  while (len >= 0) {
+    result.push(string.substr(len, 2));
+    len -= 2;
+  }
+  return result.join('');
 }
 
 const roundAlloc = (req) => {
@@ -171,7 +434,6 @@ const roundAlloc = (req) => {
 
 function getAllocSize (sk, retAddr) {
   /* Where retAddr is the addr returned from malloc */
-  var ptrSize = 8;
   return new Promise(resolve => {
     sk.emit('read_from_address', { size: ptrSize, address: retAddr }, (data) => {
     /* Callback for addr read  */
@@ -191,11 +453,15 @@ function getContentsAt (sk, addr, size) {
 
 function malloc (sk, st, data) {
   console.log('Got malloc');
-  var ptrSize = 8;
   var retAddr = data['rax-after-call'];
   console.log('got addr ', retAddr);
   getAllocSize(sk, retAddr - ptrSize).then((allocSize) => {
-    getContentsAt(sk, retAddr - (2 * ptrSize), allocSize);
+    getContentsAt(sk, retAddr - (2 * ptrSize), allocSize).then((contents) => {
+      return {
+        addr: retAddr - (2 * ptrSize),
+        contents: contents
+      };
+    });
   });
 }
 
