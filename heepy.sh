82ebc63b5c97c43e61d3187b69ca57b2d8fca907
Nathan <nbanmp@gmail.com>
2020-05-02 14:03:17 -0700

    Tmux windows instead of splits

--- heepy.sh
+++ heepy.sh
@@ -8,7 +8,7 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 tmux new-session -d -s "heepy-42" "cd $DIR/visualizer && npm run start"
-tmux split-window -v -t "heepy-42" "python3 $DIR/backend/backend.py $1 $2"
+tmux new-window -t "heepy-42" "python3 $DIR/backend/backend.py $1 $2"
 sleep 4
 xdg-open http://localhost:3000 2>&1 >/dev/null &
 tmux attach-session -t "heepy-42"
